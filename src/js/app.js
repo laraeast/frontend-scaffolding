8b7e874b00efc7d5c90594c543fb7af709dc2e74
Ahmed Fathy <aliraqi.dev@gmail.com>
2019-10-28 12:43:54 +0200

    add rtl version review

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,18 @@ require('./bootstrap');
   // Write something
 
 })(jQuery);
+
+// Switch rtl & lre versions
+window.direction = (dir) => {
+  let styleElement = $('link[data-rtl]');
+  if (dir === 'rtl') {
+    styleElement.attr('href', styleElement.data('rtl'));
+    $('html').attr('dir', 'rtl');
+  }
+  if (dir === 'ltr') {
+    styleElement.attr('href', styleElement.data('ltr'));
+    $('html').attr('dir', 'ltr');
+  }
+
+  return false;
+};
